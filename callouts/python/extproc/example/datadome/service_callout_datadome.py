7b490e0d7d283194e371c15dd42d1791345f797d
Lauro <lauro.goncalves-da-rocha@datadome.co>
2024-06-25 18:14:56 +0200

    feat: add clientID and test datadome dictionnary with lock to store data beetween callbacks

--- callouts/python/extproc/example/datadome/service_callout_datadome.py
+++ callouts/python/extproc/example/datadome/service_callout_datadome.py
@@ -7,9 +7,9 @@ from extproc.service import callout_tools
 from envoy.config.core.v3.base_pb2 import HeaderValueOption
 actions = HeaderValueOption.HeaderAppendAction
 
-#iimport threading
-#lock = threading.Lock()
-#cookie = ""
+import threading
+lock = threading.Lock()
+datadome_cookie_dict = {}
 
 
 # DataDome constants
@@ -19,7 +19,6 @@ datadome_request_headers = {
 "Content-Type": "application/x-www-form-urlencoded",
 "User-Agent": "DataDome"
 }
-
 
 class CalloutServerExample(callout_server.CalloutServer):
 
@@ -38,10 +37,25 @@ class CalloutServerExample(callout_server.CalloutServer):
     http_headers = headers.headers.headers 
     http_headers_dic={}
     for header in http_headers:
-      http_headers_dic[header.key + '_raw'] = header.raw_value
       http_headers_dic[header.key] = header.raw_value.decode()
 
     logging.debug("DataDome : headers receives from LB %s", http_headers_dic)
+    logging.debug("DataDome : cookie logging %s", http_headers_dic.get('cookie'))
+
+    # DataDome clientID simple test: this is only to make sure this is working properly and we are only considering our cookie
+    clientId = http_headers_dic.get('cookie')
+    if clientId != None:
+      clientId = clientId.replace('datadome=','')
+
+    logging.debug("DataDome : clientID logging %s", clientId)
+
+    for value in context.__dict__:
+      logging.debug("DataDome Value: %s %s",value,context.__dict__[value])
+    logging.debug(context.details)
+    logging.debug(context.peer)
+    logging.debug(context.__str__)
+    logging.debug(context.auth_context)
+
 
     datadome_payload = {
         "Key": datadome_server_side_key,
@@ -50,22 +64,23 @@ class CalloutServerExample(callout_server.CalloutServer):
         "AcceptLanguage": "fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4",
         "APIConnectionState": "new",
         "Connection": "keep-alive",
+        "ClientID": clientId,
         "HeadersList": "Host,Connection,Pragma,Cookie,Cache-Control,User-Agent",
-        "Host": "sub.domain.com",
+        "Host": "localhost",
         "IP": "62.35.12.13",
         "Method": "GET",
         "ModuleVersion": "1.0",
         "Port": 60200,
         "Pragma": "no-cache",
-        "Protocol": "https",
+        "Protocol": "http",
         "Referer": "http://sub.domain.com/home.php",
         "Request": "/folder/file.php?param=value",
         "RequestModuleName": "service-callout",
-        "ServerHostname": "sub.domain.com",
+        "ServerHostname": "localhost",
         "ServerName": "Lauro",
         "TimeRequest": 1494584456492817,
-        "TlsProtocol": http_headers_dic.get('TLS') , # WHY IT BREAKS WHEN LOOKING FOR A FIELD THAT IS NOT DEFINED?
-        "UserAgent": http_headers_dic.get('user-agent'),#http_headers_dic.get('user-agent'),
+        "TlsProtocol": http_headers_dic.get('TLS'),
+        "UserAgent": http_headers_dic.get('user-agent'),
         "XForwardedForIP": http_headers_dic.get('X-Forwarded-For') 
     }
 
@@ -90,11 +105,13 @@ class CalloutServerExample(callout_server.CalloutServer):
             code=403,
             body=response.text,
             headers=header_pairs_list)
-    
+
+      # TODO: 400 code with immediate_response
+
       if datadome_response_code == 200 :
-        # how to add a cookie to the context? below we add to the request headers that are hitting the upstream
-        #with lock:
-        #  cookie = header_pairs_dic['Set-Cookie'] 
+        # add DataDome cookie to the dictionnary to be reused inside on_response_headers
+        with lock:
+          datadome_cookie_dict[context.__str__] = header_pairs_dic['Set-Cookie']
 
         return callout_tools.add_header_mutation(
         add=header_pairs_list,
@@ -115,8 +132,11 @@ class CalloutServerExample(callout_server.CalloutServer):
       context: ServicerContext) -> service_pb2.HeadersResponse:
     
     logging.debug("DataDome : on_response_headers")
-    #with lock:
-    #  logging.debug("DataDome cookie %s",cookie)
+
+    with lock:
+      # TODO: use get to avoid exception when cookie not present
+      datadome_cookie = datadome_cookie_dict[context.__str__]
+    logging.debug("DataDome cookie %s",datadome_cookie)
 
     http_headers = headers.headers.headers 
     http_headers_dic={}
@@ -125,10 +145,13 @@ class CalloutServerExample(callout_server.CalloutServer):
       http_headers_dic[header.key] = header.raw_value.decode()
 
     logging.debug("DataDome : headers on_response_headers %s", http_headers_dic)
+    logging.debug(context.details)
+    logging.debug(context.peer)
+    logging.debug(context.__str__)
+    logging.debug(context.auth_context)
 
-    # how to add the cookie we got on on_request_headers? 
     return callout_tools.add_header_mutation(
-        add=[('x-datadome-response', 'x-datadome-response')],
+        add=[('Set-Cookie', datadome_cookie)],
         append_action=actions.OVERWRITE_IF_EXISTS_OR_ADD)
 
 
